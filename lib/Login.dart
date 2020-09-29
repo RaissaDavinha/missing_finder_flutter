46e530d98f396dd558c3fc848484023115c278e6
Raissa <raissa.furlan@mblabs.com.br>
2020-09-29 02:08:52 -0300

    login

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,225 +1,86 @@
 import 'package:flutter/material.dart';
-import 'package:adobe_xd/pinned.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-class Login extends StatelessWidget {
-  Login({
-    Key key,
-  }) : super(key: key);
+class Login extends StatefulWidget {
+  Login({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<Login> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void _Login(String email, String password) {
+    setState(() {
+      //validate login
+    });
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    emailController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xff3f51b5),
-      body: Stack(
-        children: <Widget>[
-          Transform.translate(
-            offset: Offset(44.0, 292.0),
-            child: SvgPicture.string(
-              _svg_9frynw,
-              allowDrawingOutsideViewBox: true,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: ListView(
+          padding: EdgeInsets.all(20.0),
+          children: <Widget>[
+            Text(
+              'Login',
+              style: Theme.of(context).textTheme.headline4,
             ),
-          ),
-          Transform.translate(
-            offset: Offset(95.4, 320.0),
-            child: SizedBox(
-              width: 223.0,
-              child: Text(
-                'MISSING FINDER',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 24,
-                  color: const Color(0xff3f51b5),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.center,
+            Divider(),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: 'E-mail',
+                border: OutlineInputBorder(),
               ),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(44.0, 141.0),
-            child: Text(
-              'Você não está logado.\nPor favor, entre com sua conta.Ou crie uma...',
-              style: TextStyle(
-                fontFamily: 'Roboto',
-                fontSize: 20,
-                color: const Color(0xffffffff),
-                fontWeight: FontWeight.w700,
-              ),
-              textAlign: TextAlign.left,
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(0.0, 848.0),
-            child:
-                // Adobe XD layer: 'navbar' (group)
-                SizedBox(
-              width: 414.0,
-              height: 48.0,
-              child: Stack(
-                children: <Widget>[
-                  Container(),
-                ],
+            Divider(),
+            TextFormField(
+              obscureText: true,
+              controller: passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
               ),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(55.0, 376.0),
-            child: SizedBox(
-              width: 304.0,
-              height: 48.0,
-              child: Stack(
-                children: <Widget>[
-                  Pinned.fromSize(
-                    bounds: Rect.fromLTWH(0.0, 0.0, 304.0, 48.0),
-                    size: Size(304.0, 48.0),
-                    pinLeft: true,
-                    pinRight: true,
-                    pinTop: true,
-                    pinBottom: true,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(2.0),
-                        color: const Color(0xff3f51b5),
-                        boxShadow: [
-                          BoxShadow(
-                            color: const Color(0x3d000000),
-                            offset: Offset(0, 2),
-                            blurRadius: 2,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Pinned.fromSize(
-                    bounds: Rect.fromLTWH(122.0, 14.0, 61.0, 21.0),
-                    size: Size(304.0, 48.0),
-                    fixedWidth: true,
-                    fixedHeight: true,
-                    child:
-                        // Adobe XD layer: 'Normal' (text)
-                        Text(
-                      'ENTRAR',
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 16,
-                        color: const Color(0xffffffff),
-                        fontWeight: FontWeight.w500,
-                        height: 1.25,
-                      ),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                ],
+            Divider(),
+            Expanded(
+              flex: 0,
+              child: FlatButton(
+                onPressed: () {
+                  _Login(emailController.text, passwordController.text);
+                },
+                child: Text("Login"),
+                color: Colors.blue,
+                padding: EdgeInsets.all(10),
+                textColor: Colors.white,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0)),
               ),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(55.0, 446.0),
-            child: SizedBox(
-              width: 304.0,
-              height: 48.0,
-              child: Stack(
-                children: <Widget>[
-                  Pinned.fromSize(
-                    bounds: Rect.fromLTWH(0.0, 0.0, 304.0, 48.0),
-                    size: Size(304.0, 48.0),
-                    pinLeft: true,
-                    pinRight: true,
-                    pinTop: true,
-                    pinBottom: true,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(2.0),
-                        color: const Color(0xff3f51b5),
-                        boxShadow: [
-                          BoxShadow(
-                            color: const Color(0x3d000000),
-                            offset: Offset(0, 2),
-                            blurRadius: 2,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Pinned.fromSize(
-                    bounds: Rect.fromLTWH(110.0, 14.0, 84.0, 21.0),
-                    size: Size(304.0, 48.0),
-                    fixedWidth: true,
-                    fixedHeight: true,
-                    child:
-                        // Adobe XD layer: 'Normal' (text)
-                        Text(
-                      'REGISTRAR',
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 16,
-                        color: const Color(0xffffffff),
-                        fontWeight: FontWeight.w500,
-                        height: 1.25,
-                      ),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                ],
-              ),
+            new GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, "myRoute");
+              },
+              child: new Text("Cadastrar"),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(55.0, 520.0),
-            child: SizedBox(
-              width: 304.0,
-              height: 48.0,
-              child: Stack(
-                children: <Widget>[
-                  Pinned.fromSize(
-                    bounds: Rect.fromLTWH(0.0, 0.0, 304.0, 48.0),
-                    size: Size(304.0, 48.0),
-                    pinLeft: true,
-                    pinRight: true,
-                    pinTop: true,
-                    pinBottom: true,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(2.0),
-                        color: const Color(0xffb92d00),
-                        boxShadow: [
-                          BoxShadow(
-                            color: const Color(0x3d000000),
-                            offset: Offset(0, 2),
-                            blurRadius: 2,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Pinned.fromSize(
-                    bounds: Rect.fromLTWH(121.0, 14.0, 62.0, 21.0),
-                    size: Size(304.0, 48.0),
-                    fixedWidth: true,
-                    fixedHeight: true,
-                    child:
-                        // Adobe XD layer: 'Normal' (text)
-                        Text(
-                      'GOOGLE',
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 16,
-                        color: const Color(0xffffffff),
-                        fontWeight: FontWeight.w500,
-                        height: 1.25,
-                      ),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
-
-const String _svg_9frynw =
-    '<svg viewBox="44.0 292.0 327.0 312.0" ><path transform="translate(44.0, 292.0)" d="M 0 0 L 327 0 L 327 312 L 0 312 L 0 0 Z" fill="#ffffff" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'Login.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tela Login',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Missing finder'),
+      home: Login(title: 'Missing finder'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
 
-  @override
-  LoginPageState createState() => LoginPageState();
-}
-
-class LoginPageState extends State<MyHomePage> {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-
-  void _Login(String email, String password) {
-    setState(() {
-      //validate login
-    });
-  }
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    emailController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        child: ListView(
-          padding: EdgeInsets.all(20.0),
-          children: <Widget>[
-            Text(
-              'Login',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'E-mail',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              obscureText: true,
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              flex: 0,
-              child: FlatButton(
-                onPressed: () {
-                  _Login(emailController.text, passwordController.text);
-                },
-                child: Text("Login"),
-                color: Colors.blue,
-                padding: EdgeInsets.all(10),
-                textColor: Colors.white,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
